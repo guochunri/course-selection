b18e11dcc03cbc62d46085cf48f9b9faad97f5fe
郭春日 <uochunri@gmail.com>
2017-08-02 19:52:05 +0800

    DELETE /teacher/:id

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -4,4 +4,10 @@ class TeachersController < ApplicationController
 
     render json: @teachers.to_json(:include => :courses )
   end
+
+  def destroy
+    @teacher = Teacher.find(params[:id])
+    @teacher.destroy
+  end
+
 end
