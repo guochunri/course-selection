5f539e8723ecc4c8015475b3f6c9e0637dcd3caa
郭春日 <uochunri@gmail.com>
2017-08-02 19:30:52 +0800

    N to N

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ApplicationRecord
+  has_many :teachers, through: :teacher_relationships, source: :teacher
+  has_many :courses, through: :course_relationships, source: :course
+
+  has_many :teacher_relationships
+  has_many :course_relationships
 end
